1b3c26f388b721e638db91e4f42dcf6c30114e1a
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-07-17 23:15:59 +0900

    7/17

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,12 @@
-import { useState } from 'react';
+import Calculator from "./components/Calculator";
 
-function App() {
-  const[num1,setNum1] = useState<number>(0);
-  const[num2,setNum2] = useState<number>(0);
-  const[operator,setOperator] = useState<string>('+');
-  const[result,setResult] = useState<number | null>(null);
-
-  const calculate = () => {
-    let res : number;
-    switch (operator){
-      case'+':
-        res = num1 + num2;
-        break;
-      case'-':
-        res = num1 - num2;
-        break;
-      case'*':
-        res = num1 * num2;
-        break;
-      case'/':
-        res = num2 !== 0 ? num1 / num2 : NaN;
-        break;
-      default:
-        res = NaN;
-    }
-    setResult(isNaN(res) ? null : res);
-  };
-
+function App(){
   return (
-    <div style={{padding:'2rem', fontFamily:'sans-serif'}}>
+    <div>
       <h1>React電卓</h1>
-      <input
-        type='number'
-        value={num1}
-        onChange={(e) => setNum1(parseFloat(e.target.value))}
-      />
-      <select value={operator} onChange={(e) => setOperator(e.target.value)}>
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="*">×</option>
-        <option value="/">÷</option>
-      </select>
-      <input 
-        type="number"
-        value={num2}
-        onChange={(e)=> setNum2(parseFloat(e.target.value))} 
-      />
-      <button onClick={calculate}>計算</button>
-
-      <p>結果:{result !== null ? result : '---'}</p>
+      <Calculator />
     </div>
-    
   );
-
 }
 
 export default App;
